1acf363821f7475920937eb6fe99dd8a3c8ef094
csae8092 <p.andorfer@gmail.com>
2025-03-18 16:57:42 +0100

    mino

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 ENDPOINTS = {
     "tillich-lectures": {
         "title": "A digital edition of Paul Tillich's Lecture 'Religion and Culture', Harvard 1955-1956.",
-        "url": "http://127.0.0.1:8020/oai-pmh/"
+        "url": "https://tillichcorrespondence.github.io/tillich-lectures-static/oai-pmh/"
     }
 }
 
